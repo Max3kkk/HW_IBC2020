de1e5a68c16ad76655abda18d7e5d3a639d02b85
Max3kkk <46322078+Max3kkk@users.noreply.github.com>
2020-08-10 20:19:03 +0300

    changed back to double and fixed a "line-triangle" bug

--- 1st_day/1st_task/main.cpp
+++ 1st_day/1st_task/main.cpp
@@ -3,29 +3,31 @@
 #include <cmath>
 
 using namespace std;
-
-float S(float x1, float y1, float x2, float y2, float x3, float y3)
+// function that calculates the area of triangle
+double S(double x1, double y1, double x2, double y2, double x3, double y3)
 {
-    float a, b, c, p;
+    double a, b, c, p;
     a = sqrt(pow(x1-x2, 2) + pow(y1-y2, 2));
     b = sqrt(pow(x3-x2, 2) + pow(y2-y3, 2));
     c = sqrt(pow(x1-x3, 2) + pow(y1-y3, 2));
 
     p = (a + b + c)/2.0;
-
     return sqrt(p * (p - a) * (p - b) * (p - c));
 }
 
+//find the area through Heron's formula
 int main()
 {
-    float x1,y1,x2,y2,x3,y3;
+    double x1,y1,x2,y2,x3,y3, ans;
     cin >> x1 >> y1 >> x2 >> y2 >> x3 >> y3;
 
-    if ((x1 == x2 && x2 == x3 && x1 == x3) or (y1 == y2 && y2 == y3 && y1 == y3))
+    ans = S(x1, y1, x2, y2, x3, y3);
+
+    //if area of triangle is less then a minimum possible value of double, then it is a line
+    if (ans < 2.2250738585072014e-308)
     {
         cout << "It is not a triangle, it is a line";
         return 0;
     }
-
     cout << S(x1, y1, x2, y2, x3, y3);
 }
--- 1st_day/2nd/main.cpp
+++ 1st_day/2nd/main.cpp
@@ -2,29 +2,30 @@
 #include <cmath>
 
 using namespace std;
-
-float S(float x1, float y1, float x2, float y2, float x3, float y3)
+// function that calculates the area of triangle
+double S(double x1, double y1, double x2, double y2, double x3, double y3)
 {
-    float a, b, c, p;
+    double a, b, c, p;
     a = sqrt(pow(x1-x2, 2) + pow(y1-y2, 2));
     b = sqrt(pow(x3-x2, 2) + pow(y2-y3, 2));
     c = sqrt(pow(x1-x3, 2) + pow(y1-y3, 2));
 
     p = (a + b + c)/2.0;
-
     return sqrt(p * (p - a) * (p - b) * (p - c));
 }
-
+//we find the area of parallelogram by the sum of 2 triangles
+//summarize the areas of triangles, which consist of parallelogram's sides and the point
+//if the sums of areas are equal, then the point lies inside of parallelogram
 int main()
 {
-    float x1, y1, x2, y2, x3, y3, x4, y4, x5, y5, wholeS, sumS;
+    double x1, y1, x2, y2, x3, y3, x4, y4, x5, y5, wholeS, sumS;
     cin >> x1 >> y1 >> x2 >> y2 >> x3 >> y3 >> x4 >> y4 >> x5 >> y5;
 
     wholeS = S(x1, y1, x2, y2, x3, y3) + S(x3, y3, x4, y4, x1, y1);
     sumS = S(x1, y1, x2, y2, x5, y5) + S(x2, y2, x3, y3, x5, y5) + S(x3, y3, x4, y4, x5, y5) + S(x4, y4, x1, y1, x5, y5);
 
-//    cout << wholeS << endl << sumS << endl;
-    if (wholeS == sumS)
+    //to eliminate inaccuracy (instead of comparing it with 0.0)
+    if ((wholeS - sumS) >= 2.2250738585072014e-308)
         cout << "the point lies inside";
     else
         cout << "the point lies outside";
